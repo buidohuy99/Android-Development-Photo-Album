60cf2ce3d993343c7001730585452c57071fa0aa
johnnyshi1999 <johnnyshi99@gmail.com>
2019-11-12 11:24:55 +0700

    prototype for BrushFragment
    BrushFragment: fragment handling the brush feature in editing photo

--- app/src/main/java/com/example/myalbum/EditingPhoto/PhotoEditorHandler.java
+++ app/src/main/java/com/example/myalbum/EditingPhoto/PhotoEditorHandler.java
@@ -1,5 +1,6 @@
 package com.example.myalbum.EditingPhoto;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +16,13 @@ import java.util.ArrayList;
 import ja.burhanrashid52.photoeditor.PhotoEditor;
 import ja.burhanrashid52.photoeditor.PhotoEditorView;
 
+class BrushInfo
+{
+    int size = 20;
+    int opacity= 100;
+    int color = R.color.black;
+}
+
 public class PhotoEditorHandler extends Activity implements MainCallbacks{
 
 
@@ -26,6 +34,10 @@ public class PhotoEditorHandler extends Activity implements MainCallbacks{
 
     FragmentTransaction ft;
     AddEmojFragment emojiFragment;
+    BrushFragment brushFragment;
+    BrushInfo brushInfo;
+
+
 
 
     @Override
@@ -38,6 +50,9 @@ public class PhotoEditorHandler extends Activity implements MainCallbacks{
                 .build();
 
         addEmojiButton = findViewById(R.id.addEmojiButton);
+        addBrushButton = findViewById(R.id.addBrushButton);
+        brushInfo = new BrushInfo();
+
         ArrayList<String> emoji = PhotoEditor.getEmojis(PhotoEditorHandler.this);
         emojiFragment = AddEmojFragment.newInstance(emoji);
 
@@ -46,8 +61,25 @@ public class PhotoEditorHandler extends Activity implements MainCallbacks{
         addEmojiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                photoEditor.setBrushDrawingMode(false);
                 ft = getFragmentManager().beginTransaction();
                 ft.replace(R.id.FragmentHolder, emojiFragment);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+
+        addBrushButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onClick(View view) {
+                ft = getFragmentManager().beginTransaction();
+                brushFragment = BrushFragment.newInstance(brushInfo.size, brushInfo.opacity, brushInfo.color);
+                photoEditor.setBrushDrawingMode(true);
+                photoEditor.setBrushSize(brushInfo.size);
+                photoEditor.setOpacity(brushInfo.opacity);
+                photoEditor.setBrushColor(brushInfo.color);
+                ft.replace(R.id.FragmentHolder, brushFragment);
                 ft.addToBackStack(null);
                 ft.commit();
             }
@@ -61,7 +93,22 @@ public class PhotoEditorHandler extends Activity implements MainCallbacks{
         if (sender == "EmojiFragment")
         {
 
+
             photoEditor.addEmoji(bundle.getString("ChosenEmoji"));
+        }
+
+        if (sender == "BrushFragment")
+        {
+
+            photoEditor.setBrushSize((float)bundle.getInt("BrushSize"));
+            brushInfo.size = bundle.getInt("BrushSize");
+
+            photoEditor.setOpacity(bundle.getInt("Opacity"));
+            brushInfo.opacity = bundle.getInt("Opacity");
+
+            photoEditor.setBrushColor(bundle.getInt("Color"));
+            brushInfo.color= bundle.getInt("Color");
+
         }
     }
 }

25dda7a8af18025e23d2bc3d508ea44ea89800a0
HUNG <hunghung274@gmail.com>
2019-11-17 23:06:18 +0700

    Thumbnail cập nhật theo album(lỗi chưa hiện đc), tối ưu lại code phần xem ảnh
    (không dùng ảnh có sẵn nữa)

--- app/src/main/java/com/example/myalbum/XemAnh/ViewImageActivity.java
+++ app/src/main/java/com/example/myalbum/XemAnh/ViewImageActivity.java
@@ -2,10 +2,18 @@ package com.example.myalbum.XemAnh;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.myalbum.DAO.DatabaseHandler;
@@ -20,6 +28,8 @@ public class ViewImageActivity extends Activity {
     private List<Image> listImage;
     private ViewPager viewPager;
     private CustomAdapterViewPager customAdapterViewPager;
+    private LinearLayout thumbnailsContainer;
+    //private BitmapFactory.Options options;
 
     private void getData()
     {
@@ -46,6 +56,35 @@ public class ViewImageActivity extends Activity {
         viewPager.setAdapter(customAdapterViewPager);
         viewPager.setCurrentItem(IDImage);
 
+        thumbnailsContainer = (LinearLayout) findViewById(R.id.container);
+
+        inflateThumbnails();
+
+    }
+
+    private void inflateThumbnails() {
+        for (int i = 0; i < listImage.size(); i++) {
+            View imageLayout = getLayoutInflater().inflate(R.layout.item_thumbnails, null);
+            ImageView imageView = (ImageView) imageLayout.findViewById(R.id.thumbnail);
+            imageView.setOnClickListener(onChangePageClickListener(i));
+
+            String filepath = listImage.get(i).getUrlHinh();
+
+            Bitmap bitmap = ThumbnailUtils.extractThumbnail(BitmapFactory.decodeFile(filepath),64,64);
+            //set to image view
+            imageView.setImageBitmap(bitmap);
+            //add imageview
+            thumbnailsContainer.addView(imageLayout);
+        }
+    }
+
+    private View.OnClickListener onChangePageClickListener(final int i) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(i);
+            }
+        };
     }
 
     @Override

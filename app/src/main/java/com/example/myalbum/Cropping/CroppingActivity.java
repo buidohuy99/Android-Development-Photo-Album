c7a9566fa1b412c35610b00e863ce4c56f6c1837
johnnyshi1999 <johnnyshi99@gmail.com>
2019-12-09 19:46:07 +0700

    created cropping activity

---
+++ app/src/main/java/com/example/myalbum/Cropping/CroppingActivity.java
@@ -0,0 +1,15 @@
+package com.example.myalbum.Cropping;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.myalbum.R;
+
+public class CroppingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cropping);
+    }
+}

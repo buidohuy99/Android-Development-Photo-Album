cf200e05a8973541d833abb3530342ee1cd95ae3
ndhungw <56079549+ndhungw@users.noreply.github.com>
2019-12-14 16:32:56 +0700

    changes image cropper lib

--- app/src/main/java/com/example/myalbum/Cropping/CroppingActivity.java
+++ app/src/main/java/com/example/myalbum/Cropping/CroppingActivity.java
@@ -1,15 +1,39 @@
 package com.example.myalbum.Cropping;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.example.myalbum.DAO.DatabaseHandler;
+import com.example.myalbum.DTOs.Image;
 import com.example.myalbum.R;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 public class CroppingActivity extends Activity {
+
+    Context context;
+    int IDAlbum;
+    int IDImage;
+    Image image;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cropping);
+
+        Intent myCallerIntent = getIntent();
+        Bundle myBundle = myCallerIntent.getExtras();
+        IDAlbum = myBundle.getInt("IDAlbum");
+        IDImage = myBundle.getInt("IDImage");
+        context = this;
+
+        image = DatabaseHandler.getInstance(CroppingActivity.this).getImageAt(IDAlbum,IDImage);
+
+
+
     }
 }

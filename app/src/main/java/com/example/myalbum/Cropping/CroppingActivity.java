e7d72d93b13bf4accd85da4f3f9bca5efdeadd88
ndhungw <ndh1379@gmail.com>
2019-12-18 22:28:48 +0700

    loaded image to cropping activity using library

--- app/src/main/java/com/example/myalbum/Cropping/CroppingActivity.java
+++ app/src/main/java/com/example/myalbum/Cropping/CroppingActivity.java
@@ -9,8 +9,10 @@ import android.os.Bundle;
 import com.example.myalbum.DAO.DatabaseHandler;
 import com.example.myalbum.DTOs.Image;
 import com.example.myalbum.R;
-import com.theartofdev.edmodo.cropper.CropImage;
-import com.theartofdev.edmodo.cropper.CropImageView;
+import com.isseiaoki.simplecropview.CropImageView;
+import com.isseiaoki.simplecropview.callback.LoadCallback;
+
+import java.io.File;
 
 public class CroppingActivity extends Activity {
 
@@ -19,11 +21,13 @@ public class CroppingActivity extends Activity {
     int IDImage;
     Image image;
     Uri imageUri;
+    CropImageView mCropView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cropping);
+
 
         Intent myCallerIntent = getIntent();
         Bundle myBundle = myCallerIntent.getExtras();
@@ -32,6 +36,24 @@ public class CroppingActivity extends Activity {
         context = this;
 
         image = DatabaseHandler.getInstance(CroppingActivity.this).getImageAt(IDAlbum,IDImage);
+
+        mCropView = (CropImageView) findViewById(R.id.cropImageView);
+        File file = new File(image.getUrlHinh());
+        imageUri = Uri.fromFile(file);
+
+        mCropView.startLoad(
+
+                imageUri,
+
+                new LoadCallback() {
+                    @Override
+                    public void onError(Throwable e) { }
+
+                    @Override
+                    public void onSuccess() {}
+                });
+
+        mCropView.setCropMode(CropImageView.CropMode.FREE);
 
 
 

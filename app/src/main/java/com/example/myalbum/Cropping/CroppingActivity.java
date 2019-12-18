29a82d3fb7828a9902519a14ae28639650616875
ndhungw <ndh1379@gmail.com>
2019-12-18 22:56:00 +0700

    added cropping ratio options

--- app/src/main/java/com/example/myalbum/Cropping/CroppingActivity.java
+++ app/src/main/java/com/example/myalbum/Cropping/CroppingActivity.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.myalbum.DAO.DatabaseHandler;
 import com.example.myalbum.DTOs.Image;
@@ -23,23 +26,46 @@ public class CroppingActivity extends Activity {
     Uri imageUri;
     CropImageView mCropView;
 
+    TextView option_free;
+    TextView option_3_4;
+    TextView option_16_9;
+    TextView option_square;
+    TextView option_circular;
+    TextView option_fit;
+
+
+    void findLayoutView()
+    {
+        mCropView = (CropImageView) findViewById(R.id.cropImageView);
+        option_free = (TextView) findViewById(R.id.option_free);
+        option_3_4 = (TextView) findViewById(R.id.option_3_4);
+        option_16_9 = (TextView) findViewById(R.id.option_16_9);
+        option_square = (TextView) findViewById(R.id.option_square);
+        option_circular = (TextView) findViewById(R.id.option_circular);
+        option_fit = (TextView) findViewById(R.id.option_fit);
+
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cropping);
 
-
+        //get bundle sent from hom_activity
         Intent myCallerIntent = getIntent();
         Bundle myBundle = myCallerIntent.getExtras();
         IDAlbum = myBundle.getInt("IDAlbum");
         IDImage = myBundle.getInt("IDImage");
         context = this;
 
+        //GET SELECTED IMAGE
         image = DatabaseHandler.getInstance(CroppingActivity.this).getImageAt(IDAlbum,IDImage);
 
-        mCropView = (CropImageView) findViewById(R.id.cropImageView);
+        //MAKE URI
         File file = new File(image.getUrlHinh());
         imageUri = Uri.fromFile(file);
+
+        findLayoutView();
 
         mCropView.startLoad(
 
@@ -53,9 +79,59 @@ public class CroppingActivity extends Activity {
                     public void onSuccess() {}
                 });
 
-        mCropView.setCropMode(CropImageView.CropMode.FREE);
+        //SET LISTENER
+        option_free.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setCropMode(view);
+            }
+        });
 
+        LinearLayout layout = findViewById(R.id.cropMode);
+        for (int i = 0; i < layout.getChildCount();i++)
+        {
+            View option = layout.getChildAt(i);
+            option.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    setCropMode(view);
+                }
+            });
+        }
+    }
 
+    void setCropMode(View view) {
 
+        switch (view.getId()) {
+            case R.id.option_free: {
+                mCropView.setCropMode(CropImageView.CropMode.FREE);
+                break;
+            }
+
+            case R.id.option_3_4: {
+                mCropView.setCropMode(CropImageView.CropMode.RATIO_3_4);
+                break;
+            }
+
+            case R.id.option_16_9: {
+                mCropView.setCropMode(CropImageView.CropMode.RATIO_16_9);
+                break;
+            }
+
+            case R.id.option_square: {
+                mCropView.setCropMode(CropImageView.CropMode.SQUARE);
+                break;
+            }
+
+            case R.id.option_circular: {
+                mCropView.setCropMode(CropImageView.CropMode.CIRCLE);
+                break;
+            }
+
+            case R.id.option_fit: {
+                mCropView.setCropMode(CropImageView.CropMode.FIT_IMAGE);
+                break;
+            }
+        }
     }
 }
